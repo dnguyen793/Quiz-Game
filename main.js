8a1dc3bba820265bcf567f001696dca90c88b18e
Keaton Krieger <bajabuggy313@gmail.com>
2018-03-21 11:49:58 -0700

    got check working

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
+$(document).ready(initalizeApp);
+
+function initalizeApp() {
+    debugger;
+    let test = new quiz();
+    $("#button").on('click',function () {
+        test.checkAnswers();
+        console.log(test.counter)
+    })
+}
+
 class quiz {
     constructor(){
-
+    this.counter = 0;
     }
     checkAnswers(){
-
+        var question1selceted = $('input[name=question1]:checked').val();
+        var question2selceted = $('input[name=question2]:checked').val();
+        var question3selceted = $('input[name=question3]:checked').val();
+        console.log(question1selceted,question2selceted,question3selceted);
+        if(question1selceted === 'html'){
+            this.counter++
+        }
+        if(question2selceted === 'PHP'){
+            this.counter++
+        }
+        if(question3selceted === 'javascript'){
+            this.counter++
+        }
     }
 
     displayMessage(){
@@ -15,3 +38,5 @@ class quiz {
     }
 
 }
+
+
